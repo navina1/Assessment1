441e2ffda4281a9776ccc1b2749391ad798bde10
Navina K <navinak321@gmail.com>
2024-07-11 22:08:15 +0530

    header part designing completed

--- assessment/src/components/ChartPage.jsx
+++ assessment/src/components/ChartPage.jsx
@@ -1,27 +1,23 @@
 import React from 'react'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ReferenceLine} from 'recharts';
 
-const data = [
-  { name: 'Day 1', value: 4000 },
-  { name: 'Day 2', value: 3000 },
-  { name: 'Day 3', value: 2000 },
-  { name: 'Day 4', value: 2780 },
-  { name: 'Day 5', value: 1890 },
-  { name: 'Day 6', value: 2390 },
-  { name: 'Day 7', value: 3490 },
-];
+const data = Array.from({ length: 50 }, (_, i) => ({
+    name: `${i + 1}`,
+    value: Math.floor(Math.random() * 10000), // Random values between 0 and 10000
+  }));
 
 function ChartPage() {
   return (
     <div className="chart-container">
       <h2>Chart</h2>
-      <ResponsiveContainer width="90%" height={300}>
+      <ResponsiveContainer width="80%" height={300}>
         <LineChart data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
           <Legend />
+          <ReferenceLine y={2500} stroke="#000" strokeDasharray="5 5" label="2500" />
           <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
         </LineChart>
       </ResponsiveContainer>
